0b0675c0a437a1890d7589cfb0843a7b52a13c83
kg-bsg-ox <90192244+kg-bsg-ox@users.noreply.github.com>
2021-12-14 21:22:12 +0000

    Update sysrequirements.sh
    Enabling script

--- r-code/sysrequirements.sh
+++ r-code/sysrequirements.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#sudo apt-get install libcurl4-openssl-dev
-#sudo apt install -y libudunits2-0 libudunits2-dev
-#sudo apt install libgdal-dev
-#Rscript -e 'install.packages("remotes")'
-#Rscript -e 'remotes::install_cran(c("tidyr", "dplyr", "ggplot2", "scales", "tidyverse", "lubridate", "here", "curl", "readr"))'
+sudo apt-get install libcurl4-openssl-dev
+sudo apt install -y libudunits2-0 libudunits2-dev
+sudo apt install libgdal-dev
+Rscript -e 'install.packages("remotes")'
+Rscript -e 'remotes::install_cran(c("tidyr", "dplyr", "ggplot2", "scales", "tidyverse", "lubridate", "here", "curl", "readr"))'
