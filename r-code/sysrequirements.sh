956a0e794fc86c9d9bfb70ae02d52bea3940815e
Toby Phillips <59369581+TobyPhillips@users.noreply.github.com>
2023-02-01 02:07:50 +1100

    Update sysrequirements.sh

--- r-code/sysrequirements.sh
+++ r-code/sysrequirements.sh
@@ -3,4 +3,4 @@ sudo apt-get install libcurl4-openssl-dev
 sudo apt install -y libudunits2-0 libudunits2-dev
 sudo apt install libgdal-dev
 Rscript -e 'install.packages("remotes")'
-Rscript -e 'remotes::install_cran(c("tidyr", "dplyr", "ggplot2", "scales", "tidyverse", "lubridate", "here", "curl", "readr", "writexl", "openxlsx", "sf", "ggthemes", "zoo", "rgeos", "janitor"))'
+Rscript -e 'remotes::install_cran(c("tidyr", "tidyverse", "dplyr", "ggplot2", "scales", "tidyverse", "lubridate", "here", "curl", "readr", "writexl", "openxlsx", "sf", "ggthemes", "zoo", "rgeos", "janitor"))'
